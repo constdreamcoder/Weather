b0de3cb223c1ff93b08c346049111d92e1a4be18
constdreamcoder <scdreamricher@gmail.com>
2024-09-24 21:15:24 +0900

    [FEAT] 검색 김본 화면 구현

--- Weather/Weather/Sources/Feature/Search/SearchView.swift
+++ Weather/Weather/Sources/Feature/Search/SearchView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var text: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SearchBar(text: $text)
+                .padding(.horizontal, 16)
+            
+            List(0..<20) { _ in
+                VStack(alignment: .leading) {
+                    Text("Seoul")
+                        .fontWeight(.bold)
+                    Text("KR")
+                }
+                .foregroundStyle(.white)
+                .listRowBackground(Color.blue.opacity(0))
+                .listRowSeparatorTint(.white)
+            }
+            .padding(.trailing, 16)
+            .listStyle(.plain)
+        }
+        .background(.blue.opacity(0.6))
+        .scrollIndicators(.hidden)
     }
 }
 
