f42773007b89e0f4f85348ac07afb6fabe96e4eb
constdreamcoder <scdreamricher@gmail.com>
2024-09-24 18:37:44 +0900

    [BUILD] 프로젝트 기본 구조 구성

---
+++ Weather/Weather/Sources/Feature/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Weather
+//
+//  Created by SUCHAN CHANG on 9/24/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
