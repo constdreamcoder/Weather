8d1c8e147be5aa903c8554d35f9b49c77e382641
constdreamcoder <scdreamricher@gmail.com>
2024-09-24 20:59:08 +0900

    [REFACTOR] SearchBar 코드 분리

--- Weather/Weather/Sources/Feature/Main/MainView.swift
+++ Weather/Weather/Sources/Feature/Main/MainView.swift
@@ -189,32 +189,3 @@ struct WeatherInfoView: View {
             .foregroundStyle(.white)
     }
 }
-
-struct SearchBar: View {
-    
-    @Binding var text: String
-    
-    var body: some View {
-        HStack {
-            Image(systemName: "magnifyingglass")
-            
-            TextField("Search", text: $text)
-                .foregroundStyle(.primary)
-            
-            if !text.isEmpty {
-                Button(action: {
-                    text = ""
-                }, label: {
-                    Image(systemName: "xmark.circle.fill")
-                })
-            } else {
-                EmptyView()
-            }
-        }
-        .padding(.vertical, 8)
-        .padding(.horizontal, 8)
-        .foregroundStyle(.secondary)
-        .background(Color(.secondarySystemBackground))
-        .cornerRadius(8, corners: .allCorners)
-    }
-}
