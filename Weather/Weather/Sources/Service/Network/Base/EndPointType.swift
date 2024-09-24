8b7ec7541266ff8186f3c445d240578b873b7d93
constdreamcoder <scdreamricher@gmail.com>
2024-09-24 23:52:44 +0900

    [FEAT] EndPointType 정의

---
+++ Weather/Weather/Sources/Service/Network/Base/EndPointType.swift
@@ -0,0 +1,34 @@
+//
+//  EndPointType.swift
+//  Weather
+//
+//  Created by SUCHAN CHANG on 9/24/24.
+//
+
+import Foundation
+
+typealias HTTPHeaders = [String: String]
+typealias Parameters = [String: Any?]
+
+protocol EndPointType {
+    var baseURL: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var task: HTTPTask { get }
+    var headers: HTTPHeaders { get }
+    
+    func buildURLRequest() throws -> URLRequest
+}
+
+extension EndPointType {
+    func buildURLRequest() throws -> URLRequest {
+        var url: URL = baseURL
+        url.appendPathComponent(path)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod.rawValue
+        request.allHTTPHeaderFields = headers
+        
+        return request
+    }
+}
