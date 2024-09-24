da0a6fef3706d388fa796eebb35b36b7bf814760
constdreamcoder <scdreamricher@gmail.com>
2024-09-24 23:15:21 +0900

    [FEAT] 검색 로직 구현

--- Weather/Weather/Sources/Feature/Search/SearchView.swift
+++ Weather/Weather/Sources/Feature/Search/SearchView.swift
@@ -8,24 +8,33 @@
 import SwiftUI
 
 struct SearchView: View {
+    
     @State private var text: String = ""
+    
+    var filteredCityList: [City] {
+        City.loadCityList().filter { $0.name.hasPrefix(text) || text == "" }
+    }
+    
     var body: some View {
         VStack {
             SearchBar(text: $text)
                 .padding(.horizontal, 16)
             
             List {
-                ForEach(City.loadCityList(), id: \.id) { city in
-                    VStack(alignment: .leading) {
-                        Text(city.name)
-                            .fontWeight(.bold)
-                        Text(city.country)
-                    }
-                    .foregroundStyle(.white)
+                ForEach(filteredCityList, id: \.id) { city in
+                    Button(action: {
+                        print(city.name)
+                    }, label: {
+                        VStack(alignment: .leading) {
+                            Text(city.name)
+                                .fontWeight(.bold)
+                            Text(city.country)
+                        }
+                        .foregroundStyle(.white)
+                    })
                     .listRowBackground(Color.blue.opacity(0))
                     .listRowSeparatorTint(.white)
                 }
-                
             }
             .padding(.trailing, 16)
             .listStyle(.plain)
