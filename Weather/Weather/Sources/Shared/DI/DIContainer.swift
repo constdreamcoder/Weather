fe157628780f6b4238dbfe1fa1e5d0ee5b92c756
constdreamcoder <scdreamricher@gmail.com>
2024-09-27 00:50:24 +0900

    [FEAT] DIContainer 정의

---
+++ Weather/Weather/Sources/Shared/DI/DIContainer.swift
@@ -0,0 +1,28 @@
+//
+//  DIContainer.swift
+//  Weather
+//
+//  Created by SUCHAN CHANG on 9/27/24.
+//
+
+import Foundation
+
+final class DIContainer {
+    
+    private var storage: [String: Any] = [:]
+    
+    static let shared = DIContainer()
+    private init() {}
+    
+    func register<T>(_ type: T.Type, object: T) {
+        storage["\(type)"] = object
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T {
+        guard let object = storage["\(type)"] as? T else {
+            fatalError("\(type)에 해당하는 객체가 등록되지 않았습니다.")
+        }
+        
+        return object
+    }
+}
