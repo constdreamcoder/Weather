8d928941d757ff20879f67d0d4ab439e5828b82c
constdreamcoder <scdreamricher@gmail.com>
2024-09-29 00:47:36 +0900

    [CHORE] JSONLoader 파일 이동

--- Weather/Weather/Sources/Shared/JSONLoader/JSONLoader.swift
+++ Weather/Weather/Sources/Shared/JSONLoader/JSONLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct JSONLoader {
     static func loadJSON(from fileName: String) -> Data? {
-        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json") 
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json")
         else {
             return nil
         }
