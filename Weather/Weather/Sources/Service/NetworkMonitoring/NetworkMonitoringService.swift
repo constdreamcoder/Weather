f7500782a05b102345362dae806b2f9b42b39792
constdreamcoder <scdreamricher@gmail.com>
2024-09-28 20:31:42 +0900

    [FEAT] 네트워크 모니터링 서비스 정의

---
+++ Weather/Weather/Sources/Service/NetworkMonitoring/NetworkMonitoringService.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkMonitor.swift
+//  Weather
+//
+//  Created by SUCHAN CHANG on 9/28/24.
+//
+
+import Foundation
+import Network
+import Combine
+
+protocol NetworkMonitoringServiceProtocol {
+    var isConnected: CurrentValueSubject<Bool, Never> { get }
+}
+
+final class NetworkMonitoringService: NetworkMonitoringServiceProtocol {
+    
+    var isConnected = CurrentValueSubject<Bool, Never>(true)
+    
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitorQueue", qos: .background)
+    
+    init() {
+        monitor.pathUpdateHandler = {  path in
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                isConnected.send(path.status == .satisfied)
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    deinit {
+        monitor.cancel()
+    }
+}
